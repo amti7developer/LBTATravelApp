cc58d59433994d280ec2ae991d1a222a51557f3d
Kamil <amti7.developer@gmail.com>
2023-11-20 23:23:48 +0100

    Initial Commit

---
+++ LBTATravelApp/LBTATravelAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LBTATravelAppApp.swift
+//  LBTATravelApp
+//
+//  Created by Kamil on 20/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct LBTATravelAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
